1f0068cbc86b674f2d9d9893181d202c49593758
Vladislav Matveevski <v.l.matveev@mail.ru>
2025-03-31 00:02:53 +0300

    fix

--- src/config.py
+++ src/config.py
@@ -5,6 +5,7 @@ import dotenv
 
 base_dir = Path(__file__).parent
 config_dir = base_dir.joinpath("config")
+data_dir = base_dir.joinpath("data")
 media_dir = base_dir.joinpath("media")
 
 
@@ -18,6 +19,6 @@ def read_yaml_file(file_path):
 cfg = read_yaml_file(config_dir.joinpath("config.yml"))
 bot_msg = read_yaml_file(config_dir.joinpath("bot_messages.yml"))
 bot_btn = read_yaml_file(config_dir.joinpath("bot_buttons.yml"))
-courses = read_yaml_file(config_dir.joinpath("courses.yml"))
+courses = read_yaml_file(data_dir.joinpath("courses.yml"))
 
 config_env = dotenv.dotenv_values(config_dir / ".env")
