7243b538134edeb26c291195d6431b0a61c2565d
Vladislav Matveevski <v.l.matveev@mail.ru>
2025-03-30 23:16:57 +0300

    test

--- restart.sh
+++ restart.sh
@@ -1,5 +1,8 @@
+# Обновить код из репозитория
+git pull
+
 # Остановить Docker-контейнеры
 docker compose down
 
-# Перезапустить Docker-контейнеры с пересборкой
-docker compose up -d --build
+# Перезапустить Docker-контейнеры с пересборкой, указывая путь к .env файлуу
+docker compose --env-file ./config/.env up -d --build
